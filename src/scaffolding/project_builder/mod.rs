fac28d70f7058a502b991c942c3a6a14f3218620
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-05-30 10:29:33 +0200

    feat(scaffolding/builder): test initial commit

--- src/scaffolding/project_builder/mod.rs
+++ src/scaffolding/project_builder/mod.rs
@@ -43,3 +43,56 @@ impl ProjectBuilder {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::scaffolding::{ProjectLanguage, ProjectType};
+    use super::*;
+
+    #[test]
+    fn test_builder_dest_folder() {
+        let builder = ProjectBuilder::new().dest_folder("/path/to/dest".to_string());
+        assert_eq!(builder.dest_folder, Some("/path/to/dest".to_string()));
+    }
+
+    #[test]
+    fn test_builder_name() {
+        let builder = ProjectBuilder::new().name("MyProject".to_string());
+        assert_eq!(builder.name, Some("MyProject".to_string()));
+    }
+
+    #[test]
+    fn test_builder_template_project() {
+        let template = ProjectTemplate::new(
+            "MyProject".to_string(),
+            ProjectLanguage::Cpp,
+            ProjectType::Executable,
+        );
+        let builder = ProjectBuilder::new().template_project(template.clone());
+        assert_eq!(builder.template_project, Some(template));
+    }
+
+    #[test]
+    fn test_builder_build() {
+        let template = ProjectTemplate::new(
+            "MyProject".to_string(),
+            ProjectLanguage::Cpp,
+            ProjectType::Executable,
+        );
+        let builder = ProjectBuilder::new()
+            .dest_folder("/path/to/dest".to_string())
+            .name("MyProject".to_string())
+            .template_project(template.clone());
+
+        assert_eq!(builder.build().is_ok(), true);
+    }
+
+    #[test]
+    fn test_builder_build_missing_fields() {
+        let builder = ProjectBuilder::new()
+            .dest_folder("/path/to/dest".to_string())
+            .name("MyProject".to_string());
+
+        assert_eq!(builder.build().is_err(), true);
+    }
+}
--- src/scaffolding/project_template/template_descriptor.rs
+++ src/scaffolding/project_template/template_descriptor.rs
@@ -1,8 +1,19 @@
+#[derive(Debug, PartialEq)]
 pub enum ProjectLanguage {
     Cpp,
     C,
 }
 
+impl Clone for ProjectLanguage {
+    fn clone(&self) -> Self {
+        match self {
+            Self::Cpp => Self::Cpp,
+            Self::C => Self::C,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum ProjectType {
     Library,
     Executable,
@@ -10,10 +21,32 @@ pub enum ProjectType {
     Web,
 }
 
+impl Clone for ProjectType {
+    fn clone(&self) -> Self {
+        match self {
+            Self::Library => Self::Library,
+            Self::Executable => Self::Executable,
+            Self::Console => Self::Console,
+            Self::Web => Self::Web,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct ProjectTemplate {
     name: String,
     project_language: ProjectLanguage,
     project_type: ProjectType,
+}
+
+impl Clone for ProjectTemplate {
+    fn clone(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            project_language: self.project_language.clone(),
+            project_type: self.project_type.clone(),
+        }
+    }
 }
 
 impl ProjectTemplate {
